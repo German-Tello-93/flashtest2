2e0e15e8c5a57d3b49f5437b6c4916aef2d8aa85
German-Tello-93 <german.tello.93@gmail.com>
2024-06-21 17:05:57 -0300

    se sube test flash

---
+++ testflash2/Core/Src/main.c
@@ -0,0 +1,297 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "FlASH_PAGE_F1.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define	PAGE_ADDR_1	0x0801FC00
+#define FLASH_BUFFER_SIZE 64
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+typedef struct {
+	uint32_t accion;
+	uint32_t nroUsusario;
+	uint32_t clave;
+	uint32_t nroIntentos;
+} usuario;
+
+QueueHandle_t cmd;
+QueueHandle_t data;
+
+
+uint32_t buffer[FLASH_BUFFER_SIZE];
+uint32_t datos[4];
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+static void t_flashtask(void *pvParameters){
+
+	uint32_t aux_ADDR=PAGE_ADDR_1;
+	uint32_t index=0;
+	usuario value;
+	//flashdata response;
+
+	while(1){
+		xQueueReceive(cmd, &value, portMAX_DELAY);
+		switch(value.accion){
+
+		case 1:		//lectura de un valor en particular
+			//aux_ADDR = PAGE_ADDR_1 + value.nroUsusario * 2;
+			index=(2*value.nroUsusario)+1;
+			Flash_Read_Data(aux_ADDR, buffer, FLASH_BUFFER_SIZE); //cambiar a 5 el numero de palabras en caso que se desee cambiar por datos en char
+
+			//response.id = (uint8_t)buffer[0];
+
+			value.clave = buffer[index];
+
+			xQueueSend(data,&value,portMAX_DELAY);
+			break;
+		case 2:		//escritura de un codigo nuevo en la memoria
+			Flash_Read_Data(PAGE_ADDR_1, buffer, FLASH_BUFFER_SIZE);
+			buffer[value.nroUsusario*2]=value.nroUsusario;
+			buffer[value.nroUsusario*2+1]=value.clave;
+			Flash_Write_Data(PAGE_ADDR_1, buffer, FLASH_BUFFER_SIZE);
+			break;
+
+		}
+	}
+}
+
+
+static void auxtask(void *pvParameters){
+	usuario a;
+	a.nroUsusario=0;
+	uint8_t i;
+	//a.clave=9999;
+	//a.nroIntentos=0;
+	while(1){
+		/*a.accion=2;
+		xQueueSendToBack(flash_cmd_queue, &a, portMAX_DELAY);
+		//vTaskDelay(5);*/
+		a.accion=1;
+		for(i=0;i<4;i++){
+
+		xQueueSend(cmd, &a, portMAX_DELAY);
+		//taskYIELD();
+		vTaskDelay(5/portTICK_PERIOD_MS);
+		xQueueReceive(data, &a, portMAX_DELAY);
+		a.nroUsusario++;
+
+		vTaskDelay(5/portTICK_PERIOD_MS);
+		}
+	}
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+
+  cmd = xQueueCreate(1,sizeof(usuario));
+    data = xQueueCreate(1,sizeof(usuario));
+
+    xTaskCreate(t_flashtask, "", 256, NULL, 2, NULL);
+    xTaskCreate(auxtask, "", 256, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+ /**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ testflash2/Debug/Core/Src/main.d
@@ -0,0 +1,111 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ ../Core/Inc/stm32f1xx_hal_conf.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ ../Drivers/CMSIS/Include/core_cm3.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ ../Core/Inc/FlASH_PAGE_F1.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/FreeRTOS.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/FreeRTOSConfig.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/projdefs.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/portable.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/deprecated_definitions.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/portmacro.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/mpu_wrappers.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/task.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/list.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/queue.h \
+ C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/task.h
+
+../Core/Inc/main.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+../Core/Inc/stm32f1xx_hal_conf.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+../Drivers/CMSIS/Include/core_cm3.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+
+../Core/Inc/FlASH_PAGE_F1.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/FreeRTOS.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/FreeRTOSConfig.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/projdefs.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/portable.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/deprecated_definitions.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/portmacro.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/mpu_wrappers.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/task.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/list.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/queue.h:
+
+C:/Users/germa/STM32CubeIDE/workspace_1.6.1/FreeRTOS/Inc/task.h:
